9b632926f2993a15679ab91c75c361c17a85e33f
rajakumarig <146127705+rajakumarig@users.noreply.github.com>
2023-10-03 09:23:33 +0530

    Add files via upload

---
+++ LoginChromeBrowser.java
@@ -0,0 +1,14 @@
+package seleniumbasicstestleaf;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class LoginChromeBrowser {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		ChromeDriver driver=new ChromeDriver(); 
+		driver.get("http://leaftaps.com/opentaps/control/main");
+
+	}
+
+}
